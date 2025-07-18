bc4c0add0780c5363f6a85a91cc6aed8c699bdf5
seifedach <seifeddine.achour@etudiant-enit.utm.tn>
2025-07-18 16:11:12 +0100

    globop

---
+++ globop/__init__.py
@@ -0,0 +1,15 @@
+from .optimizers import (
+    SuperSGD,
+    SuperAdam,
+    SuperAdamW,
+    SuperAdagrad,
+    SuperRMSprop
+)
+
+__all__ = [
+    "SuperSGD",
+    "SuperAdam",
+    "SuperAdamW",
+    "SuperAdagrad",
+    "SuperRMSprop"
+]
